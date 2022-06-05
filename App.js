e7100e08cae8c6b5b10896eebcdc4dea2c7a9498
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-05 22:52:37 +0200

    set env

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
