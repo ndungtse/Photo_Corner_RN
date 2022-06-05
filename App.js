93373ff4981da7a745f2cbfd680b760ff7e44d62
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-05 23:20:52 +0200

    sec

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.search}>
+          <Text style={styles.logo}>Photo Corner</Text>
+          <FontAwesome name='search' />
+      </View>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        flex: 1,
+        flexDirection: 'column'
+    },
+    search: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between' 
+    }
+})
